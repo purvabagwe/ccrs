99894dff372219513aec759bfbda423761b75e6a
purvabagwe <purvabagwe2005@gmail.com>
2026-09-13 13:25:07 +0530

    Separate frontend from backend

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 

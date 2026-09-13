94af606aa90c72812f09da3052bf13f758dd4234
purvabagwe <purvabagwe2005@gmail.com>
2026-09-13 13:44:26 +0530

    Responsive

--- server.js
+++ server.js
@@ -22,11 +22,12 @@ app.use(cors({
   origin: "*"
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "frontend")));
 app.use(express.static(path.join(__dirname)));
 
 // --- Serve Frontend ---
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 // --- Health / Status Endpoint ---
